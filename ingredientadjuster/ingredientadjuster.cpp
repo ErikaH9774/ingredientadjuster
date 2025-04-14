95c0a030d9113a1a3298ef45f18e3c6e8a22017b
ErikaH92 <erv27240@email.vccs.edu>
2025-04-14 17:37:36 -0400

    finsihed code

--- ingredientadjuster/ingredientadjuster.cpp
+++ ingredientadjuster/ingredientadjuster.cpp
@@ -7,7 +7,7 @@ namespace std {;
 
 int main()
 {
-   
+    const int initial_cookie = 48;
     int number_cookie = 0;
     double cups_of_sugar = 1.5;
    double cups_of_butter = 1;
@@ -19,9 +19,16 @@ int main()
        int cups_need = (cups_of_sugar / number_cookie) + (cups_of_butter / number_cookie) + (cups_of_flower / number_cookie);
        cout << " You will need" << cups_need << " cups of sugar \n";
 
+       double factor_cookies = number_cookie / static_cast<double>(initial_cookie);
 
+       double sugar_need = cups_of_sugar * factor_cookies; 
+       double butter_need = cups_of_butter * factor_cookies;
+       double flower_need = cups_of_flower * factor_cookies; 
 
-
+       cout << "You will need: \n;"
+       cout << sugar_need << "cups of sugar \n" << ;
+       cout << butter_need << "cups of butter \n" << ;
+       cout << flower_need << "cups of flower\n" << ;
 
     return 0;
 
